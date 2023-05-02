4580d5a2e3d7ba977a843aa21dbd8e47596a6470
tzzle <130739462+tzzle@users.noreply.github.com>
2023-05-03 00:55:42 +0300

    Add files via upload

---
+++ code-streak/index2.js
@@ -0,0 +1,119 @@
+function tick1() {
+    document.getElementById('tick-1').style.backgroundColor = 'green';
+}
+
+function tick2() {
+    document.getElementById('tick-2').style.backgroundColor = 'green';
+}
+
+function tick3() {
+    document.getElementById('tick-3').style.backgroundColor = 'green';
+}
+
+function tick4() {
+    document.getElementById('tick-4').style.backgroundColor = 'green';
+}
+
+function tick5() {
+    document.getElementById('tick-5').style.backgroundColor = 'green';
+}
+
+function tick6() {
+    document.getElementById('tick-6').style.backgroundColor = 'green';
+}
+
+function tick7() {
+    document.getElementById('tick-7').style.backgroundColor = 'green';
+}
+
+function tick8() {
+    document.getElementById('tick-8').style.backgroundColor = 'green';
+}
+
+function tick9() {
+    document.getElementById('tick-9').style.backgroundColor = 'green';
+}
+
+function tick10() {
+    document.getElementById('tick-10').style.backgroundColor = 'green';
+}
+
+function tick11() {
+    document.getElementById('tick-11').style.backgroundColor = 'green';
+}
+
+function tick12() {
+    document.getElementById('tick-12').style.backgroundColor = 'green';
+}
+
+function tick13() {
+    document.getElementById('tick-13').style.backgroundColor = 'green';
+}
+
+function tick14() {
+    document.getElementById('tick-14').style.backgroundColor = 'green';
+}
+
+function tick15() {
+    document.getElementById('tick-15').style.backgroundColor = 'green';
+}
+
+function tick16() {
+    document.getElementById('tick-16').style.backgroundColor = 'green';
+}
+
+function tick17() {
+    document.getElementById('tick-17').style.backgroundColor = 'green';
+}
+
+function tick18() {
+    document.getElementById('tick-18').style.backgroundColor = 'green';
+}
+
+function tick19() {
+    document.getElementById('tick-19').style.backgroundColor = 'green';
+}
+
+function tick20() {
+    document.getElementById('tick-20').style.backgroundColor = 'green';
+}
+
+function tick21() {
+    document.getElementById('tick-21').style.backgroundColor = 'green';
+}
+
+function tick22() {
+    document.getElementById('tick-22').style.backgroundColor = 'green';
+}
+
+function tick23() {
+    document.getElementById('tick-23').style.backgroundColor = 'green';
+}
+
+function tick24() {
+    document.getElementById('tick-24').style.backgroundColor = 'green';
+}
+
+function tick25() {
+    document.getElementById('tick-25').style.backgroundColor = 'green';
+}
+
+function tick26() {
+    document.getElementById('tick-26').style.backgroundColor = 'green';
+}
+
+function tick27() {
+    document.getElementById('tick-27').style.backgroundColor = 'green';
+}
+
+function tick28() {
+    document.getElementById('tick-28').style.backgroundColor = 'green';
+}
+
+function tick29() {
+    document.getElementById('tick-29').style.backgroundColor = 'green';
+}
+
+function tick30() {
+    document.getElementById('tick-30').style.backgroundColor = 'green';
+}
